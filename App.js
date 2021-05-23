519d81a237365f66b832fbf29efcccd510a24ac6
ngocduc <ngocductaka@gmail.com>
2021-05-23 11:03:26 +0700

    init

--- App.js
+++ App.js
@@ -3,37 +3,44 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import firebase from 'firebase';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/functions';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import AuthLoadingScreen from './screens/AuthLoadingScreen';
 import ChatScreen from './screens/ChatScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import {
-  API_KEY,
-  AUTH_DOMAIN,
-  DATABASE_URL,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
-  MEASUMENT_ID,
-} from 'react-native-dotenv';
+// import {
+//   API_KEY,
+//   AUTH_DOMAIN,
+//   DATABASE_URL,
+//   PROJECT_ID,
+//   STORAGE_BUCKET,
+//   MESSAGING_SENDER_ID,
+//   APP_ID,
+//   MEASUMENT_ID,
+// } from 'react-native-dotenv';
 
 const Stack = createStackNavigator();
 
 class App extends React.Component {
   UNSAFE_componentWillMount() {
     // Initialize Firebase
+
     var config = {
-      apiKey: API_KEY,
-      authDomain: AUTH_DOMAIN,
-      databaseURL: DATABASE_URL,
-      projectId: PROJECT_ID,
-      storageBucket: STORAGE_BUCKET,
-      messagingSenderId: MESSAGING_SENDER_ID,
-      appId: APP_ID,
-      measurementId: MEASUMENT_ID,
+      apiKey: "AIzaSyARwCWkipIU30NHnTT_DWxtkB1W_y2Nad8",
+      authDomain: "chat-da64e.firebaseapp.com",
+      databaseURL: "https://chat-da64e-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "chat-da64e",
+      storageBucket: "chat-da64e.appspot.com",
+      messagingSenderId: "999457423453",
+      appId: "1:999457423453:web:aa2af853a710dac1860db4",
+      measurementId: "G-H5PFCWF7JJ"
     };
     firebase.initializeApp(config);
   }
